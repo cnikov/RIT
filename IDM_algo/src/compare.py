0b5eebb589a4a38ae094e7c345bcad2931b9f101
cnikov <charles.lohest@student.uclouvain.be>
2023-10-23 16:04:36 +0200

    idm

---
+++ IDM_algo/src/compare.py
@@ -0,0 +1,105 @@
+import re
+from relation import *
+import pandas as pd
+
+
+def escape(string):
+    newstring = ""
+    for i in range(len(string)):
+        if string[i] == "\\" and string[i+1] == "\\":
+            newstring += "\\"
+            i += 1
+        elif string[i] == "\\":
+            pass
+        else :
+            newstring += string[i]
+    return newstring
+
+def toregex(val1):
+    regex1 = re.escape(val1)
+    if re.search(r"\\\.\\\.\\\.", regex1):
+        regex1 =  re.sub(r"\\\.\\\.\\\.", r".*", regex1)
+    if re.search(r"\\\$", regex1):
+        regex1 =  re.sub(r"\\\$[A-Z]+", r".*", regex1)
+    return regex1
+
+        
+def compare(val1,val2):
+    # check wether val1 is a regex or not :
+    isRegex = False 
+    isRegex2 = False
+    escape2= ""
+    if val1[0:2] =="r:":
+        isRegex = True
+        val1 = val1[2:]
+    if val2[0:2] == "r:" :
+        isRegex2 = True
+        val2 = val2[2:]
+        escape2 = escape(val2)
+    if not isRegex:
+        regex1 = toregex(val1)
+   
+    else : 
+        regex1 = val1
+    if isRegex2:
+        regex1 = str(regex1)
+        if regex1 == val2:
+            return Relation.EQUALITY.value
+        match1 = re.fullmatch(regex1, escape2)
+        if match1:
+            return Relation.OVERLAP.value
+        else :
+            return Relation.DIFFERENCE.value
+
+    elif isRegex and not isRegex2 :
+        regex2 = toregex(val2)
+        if regex1 == regex2: 
+            return Relation.EQUALITY.value
+
+        match1 = re.fullmatch(regex1, val2)
+        if match1:
+            return Relation.OVERLAP.value
+        else :
+            return Relation.DIFFERENCE.value
+    
+    match1 = re.fullmatch(regex1, val2)
+    if match1:
+        return Relation.OVERLAP.value
+    else :
+        return Relation.DIFFERENCE.value
+    # Helper methods to build rule set
+
+def interval(val1, val2):
+    result1 = _intervals_IDC_(val1, val2)
+    result2 = _intervals_IDC_(val2, val1)
+    if result1 == Relation.OVERLAP.value or result2 == Relation.OVERLAP.value:
+        return Relation.OVERLAP.value
+    return result1
+        
+    
+
+def _intervals_IDC_(val1,val2):
+    #check wheter two intervals are the same or not, if they overlap, ...
+    results = []
+    count = 0
+    for v1 in val1:
+        for v2 in val2:
+            results.append(compare(v1,v2))
+            count+=1
+    equality = 0
+    counter = 0
+    for i in results:
+        if i == Relation.EQUALITY.value:
+            equality +=1
+        if i == Relation.DIFFERENCE.value:
+            counter +=1
+    if equality == len(val1) and len(val1) == len(val2):
+        return Relation.EQUALITY.value
+    if (equality == len(val1) or equality == len(val2)) and len(val2) < len(val1):
+        return Relation.INCLUSION_IJ.value
+    if (equality == len(val1) or equality == len(val2)) and len(val2) > len(val1):
+        return Relation.INCLUSION_JI.value
+    if counter != count:
+        return Relation.OVERLAP.value
+    return Relation.DIFFERENCE.value
+   
---
+++ IDM_algo/src/parser_1.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import csv
+import numpy as np
+import re
+
+values = None
+
+def parse_val(val, key):
+    '''Parse string value and change it to appropriate type to be manipulated by RuleSet functions
+        Accepted formats: "false" and "true" case insensitive, 'nan', '', "*", string representation of pandas.Interval
+    '''
+    if val == '' or val == '*' or val.lower() == 'nan': return float('nan')
+    elif val[0] == '(' or val[0] == '[' or val[0] == ']':
+        return parse_interval(val, key)
+    elif val.lower() == 'false': return False
+    elif val.lower() == 'true': return True
+    elif type(val.lower().strip()) == str : return val.lower()
+    else:
+        try:
+            new_val = float(val)
+            return new_val
+        except ValueError:
+            raise ValueError("Value given doesn't have appropriate format: " + str(val))
+
+def parse_interval(val, key):
+    #get characteristics of interval (closed on the left-side, right-side, both or neither)
+    leftClosed = val[0] == '['
+    if not leftClosed and not (val[0] == '(' or val[0] == ']'):
+        raise ValueError("Value given doesn't have the proper format for a pandas.Interval: " + str(val))
+    rightClosed = val[len(val)-1] == ']'
+    if not rightClosed and not (val[len(val)-1] == ')' or val[len(val)-1] == '['):
+        raise ValueError("Value given doesn't have the proper format for a pandas.Interval: " + str(val))
+    if leftClosed and rightClosed: closed = 'both'
+    elif leftClosed and not rightClosed: closed = 'left'
+    elif not leftClosed and rightClosed: closed = 'right'
+    else: closed = 'neither'
+    mylist = []
+    i = 1
+    left_str = ''
+    while i < len(val):
+        if val[i] == ';' or i == len(val)-1:
+            try:
+                left_val = left_str  
+                mylist.append(left_val)
+                left_str = ''
+                i+=1
+            except ValueError:
+                raise ValueError("Value given doesn't have the proper format for being in the rule set: " + str(val))
+        else : 
+            left_str+=val[i]
+            i+=1 
+    print(key)
+    if key == "metavariable" or key == "metavariable-pattern" or key == "metavariable-pattern-not" or key == "metavariable-pattern-either":
+        #for each metavariable
+        #problem if VAL and VALUE in the same rule... need a full match but if one is shorter -->  start with the longer ones and replace them direclty
+        global values
+        keyval = {}
+        for string in mylist:  #sort metavariables' name by length 
+            metavariable = ""
+            i = 0
+            while string[i] != ":":
+                metavariable += string[i]
+                i+=1
+            regex = "("
+            regex += string[i+1:]
+            regex += ")"
+            keyval[metavariable] = regex
+        keyval = dict(sorted(keyval.items(), key=lambda x: len(x[0]), reverse=True)) #tri en fonction de la taille de la metavariable, reverse
+        
+        for meta in keyval.keys():
+            newdict = {}
+            regex = keyval[meta]
+            for i in values.keys():
+                value = values[i]
+                if type(value) == list:
+                    escaped = re.escape(re.escape(meta))
+                    newlist = []
+                    for j in range(len(value)):
+                        item = re.escape(value[j])
+                        if re.search(escaped, item):
+                            itembis = "r:"
+                            itembis += re.sub(escaped, regex, item)
+                            newlist.append(itembis)
+                        else : 
+                            newlist.append(value[j])
+                    newdict[i] = newlist
+                            
+                elif not isinstance(value,float) : #not a list but a single item
+                        escaped = re.escape(re.escape(meta))
+                        newval = re.escape(value)
+                        if re.search(escaped, newval):
+                            newval = re.sub(escaped, regex, newval)
+                            newdict[i] = newval
+                        else : 
+                            newdict[i] = value
+            values = newdict
+        return float('nan')  #comme ca on prends plus metavariable en compte :)
+    return mylist 
+
+def parse(tree_str):
+    counter = 0
+    tree_lst = []
+    tmp_str = ""
+    index = 0
+    while index < (len(tree_str)) : 
+        if(tree_str[index] == ";" and counter == 0):
+            tree_lst.append(tmp_str)
+            tmp_str = ""
+        elif tree_str[index] == "(":
+            counter+=1
+            if counter > 0:
+                tmp_str+=tree_str[index]
+        elif tree_str[index] == ")" : 
+            counter-=1
+            if counter == -1:
+                if tmp_str != "":
+                    tree_lst.append(tmp_str)
+                return tree_lst,index
+            else:
+                tmp_str+= tree_str[index]
+                
+        elif tree_str[index:index+3] == "AND" or tree_str[index:index+3] == "NOT" :
+            tree_lst.append(tree_str[index:index+3])
+            mystr, myindex = parse(tree_str[index+4:])
+            tree_lst.append(mystr)
+            index = index + myindex +5
+        elif tree_str[index:index+2] == "OR":
+            tree_lst.append(tree_str[index:index+2])
+            mystr, myindex = parse(tree_str[index+3:])
+            tree_lst.append(mystr)
+            index = index + myindex +4
+        else:
+            tmp_str+= tree_str[index]
+        index +=1
+    if tree_lst == []:
+        return [tree_str],len(tree_str)
+    return tree_lst , index
+
+def parse_csv(csv_name):
+    ''' Parse csv and returns list of dictionnaries representing the rules'''
+    rules = []
+    with open(csv_name, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        
+        for r in reader:
+            global values
+            values = r
+            for key, val in values.items():
+                if not key == "Rec" and not key == "Recommendation":
+                    if isinstance(val,str):
+                        try:
+                            values[key] = float(val)
+                        except ValueError:
+                            values[key] = val
+            rules.append(values)
+    return rules
+
+
